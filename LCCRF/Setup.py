19c95328fd46837ff897e0aba0edfec2b0c57c81
luye <yelu27@gmail.com>
2014-06-22 16:50:06 +0800

    add Setup.py

---
+++ LCCRF/Setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+setup(
+	name = 'LCCRFPy',
+	ext_modules=cythonize(
+	[
+		Extension(
+			"LCCRFPy", 
+			['LCCRFPy.pyx'], 
+			language="c++", 
+			extra_compile_args=["-std=c++11", "-IC:\\local\\boost_1_55_0"]
+			)
+	]),
+)
