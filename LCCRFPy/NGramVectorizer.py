3b8953de5c2a6e159a9d773f7bba30fbc6c80e21
luye <yelu27@gmail.com>
2014-06-28 16:46:59 +0800

    refator code of Vectorizers

--- LCCRFPy/NGramVectorizer.py
+++ LCCRFPy/NGramVectorizer.py
@@ -6,11 +6,11 @@ from LCCRFPy import X,Y,XItem
 class NGramVectorizer(Vectorizer):
     def __init__(self, ngram = 2):
         self.__ngram = ngram
-        self.__tag_count = len(Vectorizer.tagid_to_tagname)
-        self.__featurename_to_featureid = {}
+        self.tags = Vectorizer.tagid_to_tagname
+        self.features = {}
     
     def get_features(self):
-        return self.__featurename_to_featureid
+        return self.features
         
     def __make_gram(self, doc, end_pos, tag):
         gram = tag + " "
@@ -20,32 +20,22 @@ class NGramVectorizer(Vectorizer):
         return gram
     
     def fit(self, doc):
-        self.__tag_count = len(Vectorizer.tagid_to_tagname)
         for i in range(self.__ngram - 1, len(doc)):
             gram = self.__make_gram(doc, i, doc[i][1])
-            if gram not in self.__featurename_to_featureid:
+            if gram not in self.features:
                 new_featureid = Vectorizer.allocate_featureid()
-                self.__featurename_to_featureid[gram] = new_featureid
+                self.features[gram] = new_featureid
 
     def transform(self, doc, x):
-        if self.__ngram == 1:
-            for s2 in range(0, self.__tag_count):
-                gram = self.__make_gram(doc, 0, Vectorizer.get_tagname(s2))
-                if gram in self.__featurename_to_featureid:
-                    x[0, -1, s2, self.__featurename_to_featureid[gram]] = 1
-            for j in range(1, len(doc)):
-                for s2 in range(0, self.__tag_count):
-                    for s1 in range(0, self.__tag_count):
-                        gram = self.__make_gram(doc, j, Vectorizer.get_tagname(s2))
-                        if gram in self.__featurename_to_featureid:
-                            x[j, s1, s2, self.__featurename_to_featureid[gram]] = 1
-        
-        else:    
-            for j in range(self.__ngram - 1, len(doc)):
-                for s2 in range(0, self.__tag_count):
-                    for s1 in range(0, self.__tag_count):
-                        gram = self.__make_gram(doc, j, Vectorizer.get_tagname(s2))
-                        if gram in self.__featurename_to_featureid:
-                            x[j, s1, s2, self.__featurename_to_featureid[gram]] = 1
+        for j in range(self.__ngram - 1, len(doc)):
+            for s2 in self.tags.keys():
+                if j == 0:
+                    s1_range = [-1]
+                else:
+                    s1_range = self.tags.keys()
+                for s1 in s1_range:
+                    gram = self.__make_gram(doc, j, self.tags[s2])
+                    if gram in self.features:
+                        x[j, s1, s2, self.features[gram]] = 1
         return x
         
--- LCCRFPy/TransitionVectorizer.py
+++ LCCRFPy/TransitionVectorizer.py
@@ -6,30 +6,30 @@ from LCCRFPy import X,Y
 class TransitionVectorizer(Vectorizer):
     def __init__(self, ngram = 2):
         self.__ngram = ngram
-        self.__featurename_to_featureid = {}
+        self.features = {}
+        self.tags = Vectorizer.tagid_to_tagname
     
     def get_features(self):
-        return self.__featurename_to_featureid
+        return self.features
         
     def __make_transition(self, s1, s2):
         tr = s1 + " " + s2
         return tr
     
     def fit(self, doc):
-        self.__tag_count = len(Vectorizer.tagid_to_tagname)
         for i in range(1, len(doc)):
             tr = self.__make_transition(doc[i-1][1], doc[i][1])
-            if tr not in self.__featurename_to_featureid:
+            if tr not in self.features:
                 new_featureid = Vectorizer.allocate_featureid()
-                self.__featurename_to_featureid[tr] = new_featureid
+                self.features[tr] = new_featureid
 
     def transform(self, doc, x):
         for j in range(1, len(doc)):
-            for s2 in range(0, self.__tag_count):
-                for s1 in range(0, self.__tag_count):
-                    tr = self.__make_transition(Vectorizer.get_tagname(s1), Vectorizer.get_tagname(s2))                   
-                    if tr in self.__featurename_to_featureid:
-                        x[j, s1, s2, self.__featurename_to_featureid[tr]] = 1
+            for s2 in self.tags.keys():
+                for s1 in self.tags.keys():
+                    tr = self.__make_transition(self.tags[s1], self.tags[s2])                   
+                    if tr in self.features:
+                        x[j, s1, s2, self.features[tr]] = 1
         
         return x
         
