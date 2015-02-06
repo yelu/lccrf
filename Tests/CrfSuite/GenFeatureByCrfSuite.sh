365d708840e92a336d9427f2acfc6bbe0aed0c69
Ubuntu <luye@devluye.devluye.h3.internal.cloudapp.net>
2015-02-06 13:24:31 +0000

    generate input for crf suite for result comparing.

--- Tests/CrfSuite/GenFeatureByCrfSuite.sh
+++ Tests/CrfSuite/GenFeatureByCrfSuite.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cat ../TestLCCRFPy/data/testset.tsv | python GenDataForCrfSuite.py > input.crfsuite.tsv
-cat input.crfsuite.tsv | python ./chunking_unigram.py > features.crfsuite.tsv
-cat input.crfsuite.tsv | python ./chunking_bigram.py >> features.crfsuite.tsv
+cat ../TestLCCRFPy/data/testset.tsv | python GenDataForCrfSuite.py | \
+python ./chunking.py | \
+awk 'BEGIN{FS=OFS="\t"}{if(NF==2){print $1,$2} else if(NF==3){print $1,$2;print $1,$3}}' > input.crfsuite.tsv
+cat input.crfsuite.tsv | sort | uniq > features.crfsuite.tsv
+
