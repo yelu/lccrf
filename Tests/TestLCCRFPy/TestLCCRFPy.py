90b8f6513bdcf15ecab6727e2b722d2e8655a766
luye <yelu27@gmail.com>
2015-03-25 09:58:02 +0000

    correct test case

--- Tests/TestLCCRFPy/TestLCCRFPy.py
+++ Tests/TestLCCRFPy/TestLCCRFPy.py
@@ -11,8 +11,7 @@ class TestCaseLCCRF(unittest.TestCase):
         self.x[0].length = 2
         print self.x[0].length
         self.x[0, 0, -1, 0, 0] = 1
-        self.x[0, 1, 0, 1, 1] = 1
-        self.x[0, 1, 1, 1, 1] = 1
+        self.x[0, 1, -1, 1, 1] = 1
         print self.x.ToArray()
         
         self.y = Y()
@@ -28,8 +27,8 @@ class TestCaseLCCRF(unittest.TestCase):
     def test_fit(self):
         self.crf.Fit(self.x, self.y, 1000, 0.1, 0.1)
         weights = self.crf.GetWeights()
-        self.assertAlmostEqual(weights[0], 1.63906, 4)
-        self.assertAlmostEqual(weights[1], 1.63906, 4)
+        self.assertAlmostEqual(weights[0], 2.18198, 4)
+        self.assertAlmostEqual(weights[1], 2.18198, 4)
         
 if __name__ == "__main__":
     unittest.main()
